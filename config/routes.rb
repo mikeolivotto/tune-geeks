fd5ea260d6d274a0e13d16f03ad6b1d19e58472c
mikeolivotto <michaelolivotto@gmail.com>
2021-07-28 15:44:24 +1000

    Generate Artists controller & view

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   resources :profiles
   devise_for :users
   root "home#page"
+  get '/artists', to: "artists#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
