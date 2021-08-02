e65740e526e77504ce47300086f07bd9bc83c014
mikeolivotto <michaelolivotto@gmail.com>
2021-08-02 15:30:40 +1000

    Randomise feature listings

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,17 @@
 class HomeController < ApplicationController
+  before_action :feature_listings
+  
   def page
 
+
   end
+
+
+  private
+
+  def feature_listings
+      @featured = Listing.find(Listing.pluck(:id).sample(3))
+  end
+
+
 end
