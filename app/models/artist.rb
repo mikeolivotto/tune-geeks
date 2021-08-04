cb6a9c324f7be0f2314d17430d6919bd1becd846
mikeolivotto <michaelolivotto@gmail.com>
2021-08-04 14:18:29 +1000

    Add Artist list & show pages

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -7,4 +7,17 @@ class Artist < ApplicationRecord
     def self.random
         Artist.pluck(:name).sample(1)[0].titlecase
     end
+
+
+    def self.currently_listed
+        unique_artist_ids = Listing.select(:artist_id).distinct
+        unique_artists = []
+        unique_artist_ids.each do |artist|
+            unique_artists.push(Artist.find(artist.artist_id).name)
+        end
+
+        unique_artists
+
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :profiles
   devise_for :users
   root "home#page"
-  get '/artists', to: "artists#index"
+  get 'artists', to: "artists#index"
+  get 'artists/:id', to: "artists#show", as: :artist
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
