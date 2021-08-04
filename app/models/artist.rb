0aa281dc2df5e51f1864602eb4c3a04b5479cce5
mikeolivotto <michaelolivotto@gmail.com>
2021-08-04 18:26:25 +1000

    Sort Artists page alphabetically

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -13,11 +13,31 @@ class Artist < ApplicationRecord
         unique_artist_ids = Listing.select(:artist_id).distinct
         unique_artists = []
         unique_artist_ids.each do |artist|
+            # Pushes the whole artist record into unique_artists array
             unique_artists.push(Artist.find(artist.artist_id))
         end
 
         unique_artists
 
+    end
+
+    def self.sorted_artists
+        artist_names = []
+        # Use the id's to get artist names and push them into artist_names array
+        Artist.currently_listed.each do |artist|
+            artist_names.push(artist.name)
+        end
+
+        # Sort and mutate the array so artist names are alphabetical
+        artist_names = artist_names.sort
+
+        sorted_artist_array = []
+        # Build a new array of all artist records, sorted by name
+        artist_names.each do |artist|
+            sorted_artist_array.push(Artist.find_by_name(artist))
+        end
+
+        sorted_artist_array
     end
 
 end
